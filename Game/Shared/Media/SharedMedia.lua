2e28af28d6d7af340f9ace44396c75c2c62a6ef3
rootkit1337tv <128513433+rootkit1337tv@users.noreply.github.com>
2026-02-01 17:06:20 +0100

    Added NaowhMouseoverArrows

--- Game/Shared/Media/SharedMedia.lua
+++ Game/Shared/Media/SharedMedia.lua
@@ -4,11 +4,12 @@ local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU
 local MediaType_FONT = LSM.MediaType.FONT
 local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
 
-LSM:Register(MediaType_FONT, "GothamNarrowUltra",           [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\GothamNarrowUltra.ttf]],                 koKR + ruRU + zhCN + zhTW + western)
-LSM:Register(MediaType_FONT, "Naowh",                       [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\Naowh.ttf]],                             koKR + ruRU + zhCN + zhTW + western)
+LSM:Register(MediaType_FONT, "GothamNarrowUltra",       [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\GothamNarrowUltra.ttf]],                           koKR + ruRU + zhCN + zhTW + western)
+LSM:Register(MediaType_FONT, "Naowh",                   [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\Naowh.ttf]],                                       koKR + ruRU + zhCN + zhTW + western)
 
-LSM:Register(MediaType_STATUSBAR, "Melli",              [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\Melli]])
-LSM:Register(MediaType_STATUSBAR, "NaowhDetails",       [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhDetails]])
-LSM:Register(MediaType_STATUSBAR, "NaowhLeft",          [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhLeft]])
-LSM:Register(MediaType_STATUSBAR, "NaowhMouseover",     [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhMouseover]])
-LSM:Register(MediaType_STATUSBAR, "NaowhRight",         [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhRight]])
+LSM:Register(MediaType_STATUSBAR, "Melli",                  [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\Melli]])
+LSM:Register(MediaType_STATUSBAR, "NaowhDetails",           [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhDetails]])
+LSM:Register(MediaType_STATUSBAR, "NaowhLeft",              [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhLeft]])
+LSM:Register(MediaType_STATUSBAR, "NaowhMouseover",         [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhMouseover]])
+LSM:Register(MediaType_STATUSBAR, "NaowhMouseoverArrows",   [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhMouseoverArrows]])
+LSM:Register(MediaType_STATUSBAR, "NaowhRight",             [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\NaowhRight]])
