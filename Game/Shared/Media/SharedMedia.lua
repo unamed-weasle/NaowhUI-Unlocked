113f40cb95352a38c369c1e6c4643c1eb2a2bcf1
rootkit1337tv <128513433+rootkit1337tv@users.noreply.github.com>
2026-02-05 09:01:05 +0100

    Added GothamNarrowUltraAsia

--- Game/Shared/Media/SharedMedia.lua
+++ Game/Shared/Media/SharedMedia.lua
@@ -4,7 +4,8 @@ local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU
 local MediaType_FONT = LSM.MediaType.FONT
 local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
 
-LSM:Register(MediaType_FONT, "GothamNarrowUltra",       [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\GothamNarrowUltra.ttf]],                           koKR + ruRU + zhCN + zhTW + western)
+LSM:Register(MediaType_FONT, "GothamNarrowUltra",       [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\GothamNarrowUltra.ttf]],                           ruRU + western)
+LSM:Register(MediaType_FONT, "GothamNarrowUltra",       [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\GothamNarrowUltraAsia.ttf]],                       koKR + zhCN + zhTW)
 LSM:Register(MediaType_FONT, "Naowh",                   [[Interface\Addons\NaowhUI\Game\Shared\Media\Fonts\Naowh.ttf]],                                       koKR + ruRU + zhCN + zhTW + western)
 
 LSM:Register(MediaType_STATUSBAR, "Melli",                  [[Interface\Addons\NaowhUI\Game\Shared\Media\Textures\Melli]])
